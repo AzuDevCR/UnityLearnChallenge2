bae6c9b48db996fa4d09d42ddd04cfd94e9d1823
AzuDevCR <azudevcr@gmail.com>
2023-06-16 01:56:08 -0600

    Initial commit

---
+++ Assets/Challenge 2/Scripts/tests.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tests : MonoBehaviour
+{
+    public float speed = 40.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+       // Debug.Log(values);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
